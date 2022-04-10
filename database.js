ddb6e394737af548d73a7945674229b0bedca391
Laura Wu <laurawu@email.unc.edu>
2022-04-09 21:57:51 -0400

    changed variables

--- database.js
+++ database.js
@@ -11,18 +11,19 @@ if(row === undefined) {
     const sqlInit = `
     CREATE TABLE accesslog ( 
         id INTEGER PRIMARY KEY, 
-        remoteaddr VARCHAR,
-        remoteuser VARCHAR,
-        time NUMERIC,
-        method VARCHAR,
-        url VARCHAR,
-        protocol VARCHAR,
-        httpversion NUMERIC,
-        status VARCHAR, 
-        referrer VARCHAR,
-        useragent VARCHAR
+        remoteaddr TEXT,
+        remoteuser TEXT,
+        time TEXT,
+        method TEXT,
+        url TEXT,
+        protocol TEXT,
+        httpversion TEXT,
+        status TEXT, 
+        referrer TEXT,
+        useragent TEXT
     );
 `
+
 db.exec(sqlInit)
 } else {
     console.log("Log database exists.")
